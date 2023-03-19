bfbce55a4a5d0c7b984113e9073d7a7604f9df0b
Aakash <56353087+xii-aakash@users.noreply.github.com>
2023-03-19 17:10:29 +0530

    login added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import "./App.css";
 import Home from "./Home";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Login from "./Login";
 
 function App() {
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,17 +1,44 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
 import "./Login.css";
 
 function Login() {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+
+  function toggleRoute() {
+    if (email == "user123@gmail.com" && pass == "user123") {
+      navigate("/generator");
+    } else {
+      alert("User not found");
+      
+    }
+  }
   return (
     <div className="container">
       <div className="login-form">
         <h2>User Login</h2>
 
-        <input type="email" placeholder="  Email" className="inputbox" />
-        <input type="password" placeholder="  Password" className="inputbox" />
-        <Link to={"/generator"}>
-          <input type="submit" className="btn" value={"Login"} />
-        </Link>
+        <input
+          type="email"
+          placeholder="  Email"
+          className="inputbox"
+          onChange={(value) => setEmail(value.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="  Password"
+          className="inputbox"
+          onChange={(value) => setPass(value.target.value)}
+        />
+
+        <input
+          type="submit"
+          className="btn"
+          value={"Login"}
+          onClick={toggleRoute}
+        />
       </div>
     </div>
   );
